48f4d0eb2dbacf24d086dafcf5730cd3e5f8352a
Sergei Shilko <contact@sshilko.com>
2022-01-20 23:12:36 +0100

    Add Meilisearch MasterKey per tenant

--- 01_saas.meilisearch.up.sh
+++ 01_saas.meilisearch.up.sh
@@ -20,11 +20,15 @@ metadata:
     network: $NAME
 EOF
 
+#Expose Meilisearch (could have used HEML instead)
+KEY="$NAME.$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
+KEY=$KEY NAME=$NAME DNS=$DNS envsubst < $WORK_DIR/manifests/product/meilisearch/meilisearch-service.yaml.tmpl | kubectl apply --namespace=$NAME -f -
+
+echo "-------------- Your MasterKey is '$KEY'"
+echo "-------------- Your Endpoint  is 'http://${NAME}.meilisearch.${DNS}'"
+
 #Deploy Meilisearch
 kubectl apply -f $WORK_DIR/manifests/product/meilisearch/meilisearch.yaml --namespace=$NAME
-
-#Expose Meilisearch (could have used HEML instead)
-NAME=$NAME DNS=$DNS envsubst < $WORK_DIR/manifests/product/meilisearch/meilisearch-service.yaml.tmpl | kubectl apply --namespace=$NAME -f -
 
 #https://kind.sigs.k8s.io/docs/user/ingress/
 echo "K8S Waiting for Ingress ..."
@@ -45,7 +49,7 @@ echo "K8S Waiting for Ingress ..."
 #--set controller.setAsDefaultIngress=true
 ########### DOES NOT WORK IN KIND LOCAL SETUP
 #kubectl --namespace $NAME get services -o wide -w ingress-nginx-$NAME-controller
-kubectl --namespace $NAME get services -o wide
+#kubectl --namespace $NAME get services -o wide
 
 #https://kubernetes.github.io/ingress-nginx/user-guide/basic-usage/
 #https://kubernetes.io/docs/concepts/services-networking/ingress/
@@ -107,24 +111,24 @@ EOF
 
 #INGRESS-CONTROLLER => INGRESS => SERVICE => ENDPOINT => POD
 
-#kubectl delete ingress --namespace $NAME $NAME-ingress-meilisearch
-kubectl get ingress -n $NAME && kubectl describe ingress ingress-meilisearch-$NAME -n $NAME
-
-kubectl describe ingressclasses
+#kubectl describe ingressclasses
 
 kubectl wait --for=condition=Ready pods --all --namespace=$NAME
 
+#kubectl delete ingress --namespace $NAME $NAME-ingress-meilisearch
+kubectl get ingress -n $NAME && kubectl describe ingress ingress-meilisearch-$NAME -n $NAME
+
 #Did pods boot up?
-kubectl get pods --namespace $NAME -o wide
+#kubectl get pods --namespace $NAME -o wide
 
 #Did Meilisearch boot up in pod
-kubectl exec --stdin --tty meilisearch-0 --namespace $NAME -- curl -v http://127.0.0.1:7700/health
+#kubectl exec --stdin --tty meilisearch-0 --namespace $NAME -- curl -v http://127.0.0.1:7700/health
 
 #Does service exists?
-kubectl get services --namespace $NAME -o wide
+#kubectl get services --namespace $NAME -o wide
 
 #Does the service have any endpoints?
-kubectl get endpoints -n $NAME
+#kubectl get endpoints -n $NAME
 
 #Debug info
 #kubectl get pod --all-namespaces -o json | jq '.items[] | .metadata.name + " " +.status.podIP + " " + .metadata.namespace' |grep meilisearch
@@ -143,3 +147,5 @@ kubectl get endpoints -n $NAME
 #     source:
 #       namespaceSelector: network == '$NAME'
 # EOF
+
+curl -v "${NAME}.meilisearch.${DNS}/keys" -H "Authorization: Bearer $KEY"
